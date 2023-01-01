9ad9a71b4f428998beecaf9b1bd1a7709d9006b8
danijadetest <121578992+danijadetest@users.noreply.github.com>
2023-01-01 14:41:09 -0500

    Update Home.vue
    Added exclamation back

--- components/Home.vue
+++ components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Hey y'all</h1>
+        <h1>Hey y'all!</h1>
         <p> Follow me on Instagram! </p>
         <a href="https://www.instagram.com/imdanielleo/" target="_blank">
             here's the link </a>
