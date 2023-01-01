e7336460c49cb58117514fe5651b56d84f835c8a
danijadetest <121578992+danijadetest@users.noreply.github.com>
2023-01-01 14:32:43 -0500

    working on home page

---
+++ components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Hey y'all!</h1>
+        <p> Follow me on Instagram! </p>
+        <a href="https://www.instagram.com/imdanielleo/" target="_blank">
+            here's the link </a>
+        
+    </div>
+</template>
