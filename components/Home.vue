6dd4fc6eb1c24cd07ccebbc4210d7e53578b5a87
danijadetest <121578992+danijadetest@users.noreply.github.com>
2023-01-02 16:29:14 -0500

    Update Home.vue
    added some stuff i dont remember

--- components/Home.vue
+++ components/Home.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        <h1>Hey y'all!</h1>
-        <p> Follow me on Instagram! </p>
-        <a href="https://www.instagram.com/imdanielleo/" target="_blank">
-            here's the link </a>
-        
-    </div>
+	<div id="home">
+		<h1 id="homepageheading">Hey y'all!</h1>
+		<p class="homepagetext"> Follow me on Instagram </p>
+		<a href="https://www.instagram.com/imdanielleo/" target="_blank" id="instagramlink">
+			here's the link </a>
+	</div>
 </template>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+    .homepagetext{
+		font-size: 18px;
+		color: brown;
+        font-family: Roboto;
+	}
+</style>
